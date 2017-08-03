d932e7e06355fdd4e808a2168ea4ef173031e50a
Will Stevenson <william.l.stevenson@gmail.com>
2017-08-02 20:51:29 -0500

    Add files via upload

---
+++ opt/retropie/configs/all/runcommand-onend.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# edited by invader_meh (Discord @Will.I.Am#3195)
+# edited by zzeromin ( 2016-10-14 )
+# special thanks to zerocool
+# Reference:
+# runcommand of Retorpie:  https://github.com/retropie/retropie-setup/wiki/runcommand
+# edit and path: $ sudo nano /opt/retropie/configs/all/runcommand-onend.sh
+
+#sed '1,$d' /dev/shm/runcommand.log > /dev/shm/runcommand.log
+#echo "None" >&2
+#echo "None" >&2
+sudo rm /dev/shm/runcommand.log
